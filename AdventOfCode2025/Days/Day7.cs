d69bbf9c299637195531e2b536078deeaf44f57f
cam_eadie <cam.eadie@gmail.com>
2025-12-07 17:07:56 +0900

    Day 7

--- AdventOfCode2025/Days/Day7.cs
+++ AdventOfCode2025/Days/Day7.cs
@@ -2,19 +2,52 @@ namespace AdventOfCode2025.Days;
 
 public class Day7 : IDay
 {
-    // input = ["3   4", "4   3", ...]
+    // input = [".......S.......", "...............", ...]
 
     public string Part1(string[] input)
     {
-        var sum = 1;
+        var sum = 0;
+        var rays = new HashSet<int> { input[0].IndexOf('S') };
+        foreach (var line in input)
+        {
+            foreach (var ray in rays.ToArray())
+            {
+                if (line[ray] == '^')
+                {
+                    sum++;
+                    rays.Remove(ray);
+                    rays.Add(ray - 1);
+                    rays.Add(ray + 1);
+                }
+            }
+        }
 
         return sum.ToString();
     }
 
     public string Part2(string[] input)
     {
-        var sum = 2;
+        var rays = new HashSet<int> { input[0].IndexOf('S') };
+        var paths = input[0].Select(x => x == 'S' ? 1L : 0L).ToArray();
+        foreach (var line in input)
+        {
+            var newPaths = paths.ToArray();
+            foreach (var ray in rays.ToArray())
+            {
+                if (line[ray] == '^')
+                {
+                    rays.Remove(ray);
+                    rays.Add(ray - 1);
+                    rays.Add(ray + 1);
 
-        return sum.ToString();
+                    newPaths[ray] -= paths[ray];
+                    newPaths[ray - 1] += paths[ray];
+                    newPaths[ray + 1] += paths[ray];
+                }
+            }
+            paths = [.. newPaths];
+        }
+
+        return paths.Sum().ToString();
     }
 }
